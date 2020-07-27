3ee46449ce64fb9957d55a71c430d5f7b6fac140
Ksdem <ksdem9488@gmail.com>
2020-07-27 14:47:42 +0300

    render VS components

--- f01/m1/my-app/src/App.js
+++ f01/m1/my-app/src/App.js
@@ -9,7 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
-const App = () => {
+const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -18,11 +18,11 @@ const App = () => {
                 <Navbar/>
                 {/*<Profile/>*/}
                 <div class='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
-                    <Route path='/News' component={News}/>
-                    <Route path='/Music' component={Music}/>
-                    <Route path='/Settings' component={Settings}/>
+                    <Route path='/dialogs' render={()=><Dialogs/>}/>
+                    <Route path='/profile' render={()=><Profile/>}/>
+                    <Route path='/News' render={()=><News/>}/>
+                    <Route path='/Music' render={()=><Music/>}/>
+                    <Route path='/Settings' render={()=><Settings/>}/>
                 </div>
             </div>
         </BrowserRouter>)
